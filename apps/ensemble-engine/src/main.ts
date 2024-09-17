008751af99dfca1511d53906a0785cbc41a0c9ea
Bertrand Juglas <b@juglas.name>
2024-09-18 00:45:45 +0200

    add env sample

--- apps/ensemble-engine/src/main.ts
+++ apps/ensemble-engine/src/main.ts
@@ -4,7 +4,20 @@ import 'dotenv/config';
 
 async function bootstrap() {
   console.log('Starting Ensemble Engine...');
-  console.log('process.env.MONGODB_URI', process.env.MONGODB_URI);
+  console.log('process.env.MONGODB_URI:', process.env.MONGODB_URI);
+  console.log('process.env.PROVIDER_URL_FUSE:', process.env.PROVIDER_URL_FUSE);
+  console.log(
+    'process.env.PROVIDER_URL_SEPOLIA:',
+    process.env.PROVIDER_URL_SEPOLIA,
+  );
+  console.log(
+    'process.env.PROVIDER_URL_BASE_SEPOLIA:',
+    process.env.PROVIDER_URL_BASE_SEPOLIA,
+  );
+  console.log(
+    'process.env.PROVIDER_URL_OP_SEPOLIA:',
+    process.env.PROVIDER_URL_OP_SEPOLIA,
+  );
   const app = await NestFactory.createApplicationContext(EngineAppModule);
 }
 
