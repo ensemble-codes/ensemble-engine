719f86dd87e4099c1ce15b96c3d3a1e1f368d86b
Bertrand Juglas <b@juglas.name>
2024-09-18 00:15:34 +0200

    MONGODB_URI env display fix

--- apps/ensemble-engine/src/main.ts
+++ apps/ensemble-engine/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { EngineAppModule } from './ensemble-engine.module';
-import 'dotenv/config'
+import 'dotenv/config';
 
 async function bootstrap() {
   console.log('Starting Ensemble Engine...');
+  console.log('process.env.MONGODB_URI', process.env.MONGODB_URI);
   const app = await NestFactory.createApplicationContext(EngineAppModule);
 }
 
