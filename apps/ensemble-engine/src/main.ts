3aafdfa92a916e1ba06f79ad700bc2a5cb07af22
Bertrand Juglas <b@juglas.name>
2024-09-18 15:44:04 +0200

    Cleaning debug logs
    Checking URI of MongoDB should be done like URL of RPC.
    Should keep main.ts as clear as possible.

--- apps/ensemble-engine/src/main.ts
+++ apps/ensemble-engine/src/main.ts
@@ -1,23 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { EngineAppModule } from './ensemble-engine.module';
-import 'dotenv/config';
 
 async function bootstrap() {
   console.log('Starting Ensemble Engine...');
-  console.log('process.env.MONGODB_URI:', process.env.MONGODB_URI);
-  console.log('process.env.PROVIDER_URL_FUSE:', process.env.PROVIDER_URL_FUSE);
-  console.log(
-    'process.env.PROVIDER_URL_SEPOLIA:',
-    process.env.PROVIDER_URL_SEPOLIA,
-  );
-  console.log(
-    'process.env.PROVIDER_URL_BASE_SEPOLIA:',
-    process.env.PROVIDER_URL_BASE_SEPOLIA,
-  );
-  console.log(
-    'process.env.PROVIDER_URL_OP_SEPOLIA:',
-    process.env.PROVIDER_URL_OP_SEPOLIA,
-  );
   const app = await NestFactory.createApplicationContext(EngineAppModule);
 }
 
